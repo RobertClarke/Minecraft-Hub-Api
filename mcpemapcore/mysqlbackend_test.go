c1b628ed9d165d33c9ed419a1fb452489006bf41
James Clarke <james@clarkezone.net>
2016-05-17 19:29:46 -0700

    Fix test

--- mcpemapcore/mysqlbackend_test.go
+++ mcpemapcore/mysqlbackend_test.go
@@ -6,3 +6,14 @@ func TestUploadMapDownloadCount(t *testing.T) {
 	sql := MySqlBackend{}
 	sql.UpdateMapDownloadCount("b22bf60c3bf35994c606059a59ed1f99")
 }
+
+func TestUpdateFavoriteMap(t *testing.T) {
+	var u *User
+
+	var mapId string
+	mapId = "234"
+	u.Id = "123"
+
+	sql := MySqlBackend{}
+	sql.UpdateFavoriteMap(u, mapId, true)
+}
