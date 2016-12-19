262cf83f0c864e8c8bdfb064347bd1a3094851c9
root <root@dev2.minecrafthub.com>
2016-12-19 20:59:07 +0000

    metrics

--- metricsprodconfig/prod.sh
+++ metricsprodconfig/prod.sh
@@ -1,2 +1,2 @@
 #/usr/local/alertmanager-0.5.1.linux-amd64/alertmanager -configfile=/root/GoRoot/src/github.com/robertclarke/Minecraft-Hub-Api/metricsprodconfig/alertmanager.yml
-/usr/local/prometheus-1.3.1.linux-amd64/prometheus
+/usr/local/prometheus-1.3.1.linux-amd64/prometheus -config.file=prometheus.yml
